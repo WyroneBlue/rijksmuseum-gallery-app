0d281ad383f1211554b16ed9ed3d706b9c96bc6e
WyroneBlue <wyroneblue@gmail.com>
2023-02-21 15:55:19 +0100

    Update main.js
    close filters on button click

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ import { $ } from './helpers.js';
 import router from './router.js';
 
 const filterButton = $('footer > button');
+const filterClose = $('aside > section button');
 const filters = $('aside');
 
 filterButton.addEventListener('click', toggleFilters);
+filterClose.addEventListener('click', toggleFilters);
 
 export function toggleFilters (){
     filters.classList.toggle('show');
